dcf0a36a136676f7ed585547bca7fc6bb83fc47f
Juan Aguilar <naujsigma@gmail.com>
2015-11-04 00:00:17 -0600

    Curso Derivadas y prueba 2 de mejora de diseño responsivo

---
+++ cursoCálculodiferencial.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+     <link type="text/css" rel="stylesheet" href="css/style_curso1.css">
+	<title>C&aacute;lculo Diferencial</title>
+</head>
+<body style="text-aling:justify;">
+        <section id="title">C&aacute;lculo Diferencial</section>
+    	
+        <div id="Temario">
+      <br>
+      <h1><b>TEMARIO DEL CURSO</b></h1>
+       <br>
+       </div>
+      <ol id="Estilos1">
+        <li>Dedifinici&oacute;n de derivada</li>
+        <li>Representaci&oacute;n</li>
+        <li>Derivadas comunes</li>
+           <ul>
+        <li>Funciones algebraicas</li>
+
+        <li>Funciones trigonom&eacute;tricas</li>
+           </ul>
+        <li>Soluci&oacute;n a ejercicios</li>
+      </ol>
+      <br>
+      <h1><b>PARTE TE&Oacute;RICA</b></h1>
+      <div id="PT">
+      <p style="text-align:justify;">
+      <br>
+      <b>Definici&oacute;n de derivada</b>
+      <br>
+      <br>
+      La derivada de una funci&oacute;n es igual a: el l&iacute;mite,cuando el incremento de la variable independiente tiende a cero del incremento de la funci&oacute;n sobre el incremento de la variable dependiente. La siguiente ecuaci&oacute;n muestra de manera visual la definici&oacute;n anterior:
+      <br>
+      <br>
+      <div style="text-align:center;">
+      <img id="Imagen" src="img/dderivada00.png"/>
+      </div>
+      </p>
+      <br>
+      <br>
+      <p style="text-align:justify;">
+      <b>Representaci&oacute;n</b>
+      <br>
+      <br>
+      Es muy com&uacute;n encontrar una o dos maneras de representar la derivaci&oacute;n de una funci&oacute;n en este apartado mostraremos otras, para que en caso de encontrar ejercicios que usen diferentes notaciones, sepan que solamente se trata de un cambio s&iacute;mbolico, dichas notaciones se muestran en la siguiente igualdad de ecuaciones:
+      </p>
+      <br>
+      <br>
+      <div style="text-align:center;">
+      <img id="Imagen" src="img/dderivada01.png"/>
+      </div>
+      <br>
+      <br>
+       <p style="text-align:justify;">
+       <b>Derivadas comunes</b>
+      <br>
+      <br>
+       Este tipo de dervidas son la formas generales para la soluci&oacute;n de ejercicios particulares (ejercicios que pueden variar ya sea si un factor multiplica a la funci&oacute;n o en otros caso el argumento puede estar compuesto por m&aacute;s de un t&eacute;rmino).
+      </p>
+      </div>
+
+</body>
+</html>
